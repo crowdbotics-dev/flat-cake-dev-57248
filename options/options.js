be665e38910bbcde896cfa3ce12e71c7b1367d82
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 15:14:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-cake-dev-57248",
+  url: "https://flat-cake-dev-57248.botics.co",
+  api: "https://flat-cake-dev-57248.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
